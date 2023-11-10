0c0ede411e578a56489ffbc2d5605c50f599a056
hermaer <91397176+hermaer@users.noreply.github.com>
2023-11-10 13:41:31 +0700

    Create params.py

---
+++ params.py
@@ -0,0 +1,17 @@
+
+
+W, H = 1200, 800
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+x_heroi = 90
+y_heroi = 700
+FPS = 60
+speed = 5
+x_bullet = x_heroi
+speed_bullet = 15
+ground = H-70         
+jump_force = 20         
+move = jump_force+1     
